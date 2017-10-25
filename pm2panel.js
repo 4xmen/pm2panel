9ec1fe9281cddf769251f333351b137628755e13
A1Gard <a1gard@4xmen.ir>
2017-10-26 00:17:10 +0330

    add some assets make login

--- pm2panel.js
+++ pm2panel.js
@@ -10,12 +10,19 @@ const SESSTION_AGE = 10 * 60000; // 10 minutes
 //##############################################################################
 //                             inital packages
 //##############################################################################
+const path = require('path');
 const express = require('express');
 const app = express();
 var session = require('express-session');
+
 // Use the session middleware
 app.use(session({secret: 'keyboard cat', cookie: {maxAge: SESSTION_AGE}}));
+// add assets path
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+//
 //##############################################################################
 //                             rounting urls
 //##############################################################################
@@ -33,10 +40,22 @@ app.get('/', function (req, res) {
 });
 
 app.get('/login', function (req, res) {
-    res.send('login');
+    res.sendFile(path.join(__dirname, 'www/login.html'));
 });
-app.get('/loginCheck', function (req, res) {
-    req.session.islogin = true; 
+
+app.post('/loginCheck', function (req, res) {
+    if (req.body.username === USER && req.body.passwd == PASS) {
+        req.session.islogin = true;
+        res.writeHead(302, {
+            'Location': '/'
+                    //add other headers here...
+        });
+    } else {
+        res.writeHead(302, {
+            'Location': '/login'
+                    //add other headers here...
+        });
+    }
     res.end();
 });
 
