6e0572a2697da791e83372c7ae968e73556e1f1d
A1Gard <a1gard@4xmen.ir>
2017-10-26 01:55:26 +0330

    add some comment fix logout redirect bug

--- pm2panel.js
+++ pm2panel.js
@@ -10,6 +10,7 @@ const SESSTION_AGE = 10 * 60000; // 10 minutes
 //##############################################################################
 //                             inital packages
 //##############################################################################
+
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -18,20 +19,25 @@ var session = require('express-session');
 
 // Use the session middleware
 app.use(session({secret: 'keyboard cat', cookie: {maxAge: SESSTION_AGE}}));
+
 // add assets path
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
+// for parse post
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 //
 //##############################################################################
 //                             rounting urls
 //##############################################################################
+
 app.get('/', function (req, res) {
+    // check is login
     if (req.session.islogin) {
+        // show index page
         res.sendFile(path.join(__dirname, 'www/index.html'));
     } else {
-//        req.session.views = 1
+        // redirect to login page
         res.writeHead(302, {
             'Location': '/login'
         });
@@ -41,16 +47,22 @@ app.get('/', function (req, res) {
 });
 
 app.get('/login', function (req, res) {
+    
+    // render login page
     res.sendFile(path.join(__dirname, 'www/login.html'));
 });
 
 app.post('/loginCheck', function (req, res) {
+    // check username and password 
     if (req.body.username === USER && req.body.passwd == PASS) {
+        // login process
         req.session.islogin = true;
+        // redirect to panel
         res.writeHead(302, {
             'Location': '/'
         });
     } else {
+        // user or password incrrect go back to login
         res.writeHead(302, {
             'Location': '/login'
 
@@ -62,6 +74,7 @@ app.post('/loginCheck', function (req, res) {
 
 
 app.get('/getProccess', function (req, res) {
+    // check is user logined
     if (!req.session.islogin) {
         res.writeHead(302, {
             'Location': '/login'
@@ -69,11 +82,11 @@ app.get('/getProccess', function (req, res) {
         res.end();
 
     } else {
-
+        // send json header
         res.writeHead(200, {
             'Content-Type': 'application/json'
-
         });
+        // get json list from the json
         exec("pm2 jlist", (error, stdout, stderr) => {
             //do whatever here
             res.write(stdout);
@@ -83,6 +96,7 @@ app.get('/getProccess', function (req, res) {
 });
 
 app.get('/restart', function (req, res) {
+     // send json header
     if (!req.session.islogin) {
         res.writeHead(302, {
             'Location': '/login'
@@ -90,8 +104,9 @@ app.get('/restart', function (req, res) {
         res.end();
 
     } else {
-        
+        // check id exits 
         if (req.query.id) {
+            // restart the process
             exec("pm2 restart " + req.query.id, (error, stdout, stderr) => {
                 res.writeHead(302, {
                     'Location': '/'
@@ -109,7 +124,13 @@ app.get('/restart', function (req, res) {
 
 
 app.get('/logout', function (req, res) {
+    
+    // remover session
     delete req.session.islogin;
+    // redirect to login page
+    res.writeHead(302, {
+        'Location': '/login'
+    });
     res.end();
 });
 
